261e07b90e2fdeb990602a5cbf4255aa0cb9eed8
Rajneesh Kumar <rajneeshmkumar1111@gmail.com>
2025-09-20 17:56:50 +0530

    region and route type complete crud and salsman type error fix and company costumer get and delete

--- app/(private)/data/settingLinks.tsx
+++ app/(private)/data/settingLinks.tsx
@@ -110,6 +110,13 @@ export const initialLinkData: SidebarDataType[] = [
                 children: [
                     {
                         isActive: false,
+                        href: "/dashboard/settings/company/companyCustomer",
+                        label: "Company Customer",
+                        leadingIcon: "lucide:user",
+                        iconColor: "text-green-500",
+                    },
+                    {
+                        isActive: false,
                         href: "/dashboard/settings/customer/companyType",
                         label: "Company Type",
                         leadingIcon: "lucide:user",
@@ -345,6 +352,13 @@ export const initialLinkData: SidebarDataType[] = [
                 isActive: false,
                 href: "/dashboard/settings/expenseType",
                 label: "Expense Type",
+                leadingIcon: "mdi:currency-usd",
+                iconColor: "text-green-500",
+            },
+              {
+                isActive: false,
+                href: "/dashboard/settings/salesman-type",
+                label: "Salesman Type",
                 leadingIcon: "mdi:currency-usd",
                 iconColor: "text-green-500",
             },
